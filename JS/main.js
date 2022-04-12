4bbc590c02da63afae2522f3c5464d5c68a3406f
SU-T <guitaristpeet@gmail.com>
2022-04-12 15:03:45 -0700

    Hero / Licence Section Media

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,4 @@
 const headerBg = document.querySelector('.header-bg')
-
 inView('.hero')
     .on('enter', removebg => {
         headerBg.classList.remove('appear')
@@ -7,5 +6,16 @@ inView('.hero')
     .on('exit', addbg => {   
         headerBg.classList.add('appear')
     });
-
 inView.threshold(0.9);
+
+const menuBtn = document.querySelector('.menu-btn')
+let menuOpen = false;
+menuBtn.addEventListener('click', open => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
