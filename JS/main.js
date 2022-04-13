3cc29686c6f144d2112090ae8eeb3810068724a0
SU-T <guitaristpeet@gmail.com>
2022-04-12 23:48:48 -0700

    Finish Menu-box

--- JS/main.js
+++ JS/main.js
@@ -9,13 +9,31 @@ inView('.hero')
 inView.threshold(0.9);
 
 const menuBtn = document.querySelector('.menu-btn')
+const menuBox = document.querySelector('.menu')
 let menuOpen = false;
 menuBtn.addEventListener('click', open => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
+        menuBox.classList.add('visible');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
+        menuBox.classList.remove('visible');
         menuOpen = false;
     }
 });
+
+const arrow = document.querySelector('.menu-arrow')
+const menuOptions = document.querySelector('.menu-options')
+let menuSlide = false;
+arrow.addEventListener('click', slide => {
+    if (!menuSlide) {
+        menuOptions.classList.add('slide')
+        menuBox.classList.add('shrink')
+        menuSlide = true;
+    } else {
+        menuOptions.classList.remove('slide')
+        menuBox.classList.remove('shrink')
+        menuSlide = false;
+    }
+})
