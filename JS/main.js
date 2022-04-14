32df15efa2dd781e86e2c2e33c044971b6fd4d2b
SU-T <guitaristpeet@gmail.com>
2022-04-13 20:21:33 -0700

    Add Slider section in html

--- JS/main.js
+++ JS/main.js
@@ -37,3 +37,55 @@ arrow.addEventListener('click', slide => {
         menuSlide = false;
     }
 })
+
+// const slide = document.querySelector('.slide-content')
+// const slideDiv = document.querySelectorAll('.slide-content div')
+// // slideDiv.forEach((el, index) => {
+// //     position = index + 1
+// //     console.log(position);
+// // });
+// const prevBtn = document.querySelector('#prevBtn')
+// const nextBtn = document.querySelector('#nextBtn')
+
+// let position = 1;
+// const size = slideDiv[0].clientWidth;
+// // slide.style.transform = 'translateX(' + (-size * position) + 'px)';
+
+// const nextCont = slideRight => {
+//     position++;
+//     slide.style.transform = 'translateX(' + (-100 * position) + 'px)';
+//     console.log(position);
+// }
+// const prevCont = slideLeft => {
+//     position--;
+//     slide.style.transform = 'translateX(' + (100 * position) + 'px)';
+//     console.log(position);
+// }
+
+// nextBtn.addEventListener('click', nextCont);
+// prevBtn.addEventListener('click', prevCont);
+
+const slide = document.querySelector('.slide-content')
+const slideDiv = document.querySelectorAll('.slide-content div')
+const prevBtn = document.querySelector('#prevBtn')
+const nextBtn = document.querySelector('#nextBtn')
+
+const currentSlide = 0
+const totalSlide = slideDiv.length
+
+const moveSlide = function () {
+    unit = -currentSlide * 100;
+    slide.style.transform = 'translateX(' + unit + 'vw)';
+}
+
+const nextSlide = () => {
+    currentSlide += 1;
+
+    if (currentSlide >= totalSlide) {
+        currentSlide = 0
+    }
+
+    moveSlide(currentSlide);
+}
+
+nextBtn.addEventListener('click', nextSlide);
